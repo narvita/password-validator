a4326b4eb80b32b57b61eddde6ba635332f49428
narvita <narkha21@gmail.com>
2022-10-17 17:33:20 +0400

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const password = useRef(null);
+  const [isValidate, setValidation] = useState(false);
+  const pattern = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
+  );
+
+
+  let validate = isValidate ? 'True' : "false"
+
+  function StringChallenge(str) {
+    if (str.length < 7 || str.length > 31) {
+      return false;
+    } else if (str.includes("password")) {
+      return false;
+    } else if (pattern.test(str)) {
+      return true;
+    }
+    return false;
+  }
+
+  const handleClick = () => {
+    let isValid = StringChallenge(password.current.value);
+    console.log(isValid, "hhhhhh");
+    setValidation(isValid);
+
+    console.log(isValidate);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input ref={password} />
+      <button onClick={handleClick}>Check validation</button>
+      <h2>password is {validate}</h2>
     </div>
   );
 }
